e3307c555c43c65e9cf6d968344b91d8384c3e59
“devil” <wwgpbili2233@gmail.com>
2023-07-15 20:59:50 +0800

    fix bug

--- pages/video/video.js
+++ pages/video/video.js
@@ -24,8 +24,11 @@ Page({
   //获取导航数据
   async getVideoGroupListData(){
     let videoGroupListData = await request('/video/group/list');
+    videoGroupListData.data.splice(4,1)
+    videoGroupListData.data.splice(6,5)
+    //console.log(videoGroupListData)
     this.setData({
-      videoGroupList: videoGroupListData.data.slice(0,20),
+      videoGroupList: videoGroupListData.data.slice(0,14),
       navId: videoGroupListData.data[0].id
     })
     this.getVideoList(this.data.navId);
@@ -53,6 +56,7 @@ Page({
       return;
     }
     let VideoListData = await request('/video/group',{id: navId});
+    //console.log(VideoListData)
     if(VideoListData.datas.length === 0){
       wx.showToast({
         title: '暂无推荐视频',
